2a35a5dbd7495e3ca31711431b93654c7e7042a5
sangamesh.h129@gmail.com <sangamesh.hamje@gmail.com>
2022-04-08 22:45:47 +0530

    testng practice

---
+++ src/com/testng/practice/TestngPr.java
@@ -0,0 +1,18 @@
+package com.testng.practice;
+
+import org.testng.annotations.Test;
+
+public class TestngPr {
+  @Test
+  public void m1() {
+	  System.out.println("Iam from method 1");
+  }
+  @Test
+  public void m2() {
+	  System.out.println("Iam from method 2");
+  }
+  @Test
+  public void m3() {
+	  System.out.println("Iam from method 3");
+  }
+}
